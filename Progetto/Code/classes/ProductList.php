29a663529df7dd396a21e10b281dd0d275776b43
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2025-01-19 14:22:50 +0100

    add ProductList class to manage product collections

---
+++ Progetto/Code/classes/ProductList.php
@@ -0,0 +1,16 @@
+<?php
+
+class ProductList
+{
+    private $products = [];
+    public function __construct($products)
+    {
+        $this->products = $products;
+    }
+
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+}
