44d2ac7807675776b4c8fe2f931fdf37a33c7bcc
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2025-01-20 14:34:50 +0100

    add footer section to include copyright information

---
+++ Progetto/Code/includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="py-3 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <span class="text-muted">&copy; 2025 CraftMania. Tutti i diritti riservati. I marchi e i prodotti appartengono ai rispettivi creatori.</span>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
