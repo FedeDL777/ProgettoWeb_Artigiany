797c56734df03f654cf5e9e3f65a53e42459e761
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2025-01-21 13:57:04 +0100

    update footer with contact information and location details

--- Progetto/Code/includes/footer.php
+++ Progetto/Code/includes/footer.php
@@ -1,8 +1,39 @@
 <footer class="py-3 bg-light">
     <div class="container">
+        
         <div class="row">
             <div class="col-12 text-center">
-                <span class="text-muted">&copy; 2025 CraftMania. Tutti i diritti riservati. I marchi e i prodotti appartengono ai rispettivi creatori.</span>
+                <span class="text-muted"> 
+                    <i class="bi bi-geo-alt-fill"></i>Cesena, Via dell'Università 50, 
+
+                </span>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <span class="text-muted"> 
+                    <i class="bi bi-telephone-fill"></i>Tel. 0547 123456, 
+
+                </span>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <span class="text-muted"> 
+                    <i class="bi bi-envelope-at-fill"></i>Email: Artigiany@nomail.it
+
+                </span>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <span class="text-muted">&copy; 2025 CraftMania. Tutti i diritti riservati. I marchi e i prodotti appartengono ai rispettivi creatori.
+
+                </span>
+                
             </div>
         </div>
     </div>
