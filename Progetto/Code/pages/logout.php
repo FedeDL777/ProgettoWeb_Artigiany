73c94fd38a6fa7b714a77395a22201336fe77b1c
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2025-01-25 15:57:49 +0100

    corretta impaginazione logout, validazione wip

--- Progetto/Code/pages/logout.php
+++ Progetto/Code/pages/logout.php
@@ -2,21 +2,27 @@
 
 include_once("../includes/bootstrap.php");
 include_once("../includes/functions.php");
+
 if (!isUserLoggedIn() && !isAdminLoggedIn()) {
     header("Location: home.php");
 }
 logout();
 include("../includes/header.php");
 ?>
-<body>
-    <main>
-        <div class="d-flex justify-content-center align-items-center">
-            <div class="container text-center">
-                <h1>Logout effettuato</h1>
+
+<main>
+    <div class="container-page">
+        <div class="row">
+            <div class="d-flex justify-content-center align-items-center">
+                <div class="container text-center">
+                    <h1>Logout effettuato</h1>
+                    <!-- Bottone per tornare alla home -->
+                    <a href="home.php" class="btn btn-primary btn-lg mt-4">Torna alla Home</a>
+                </div>
             </div>
         </div>
-    </main>
-</body>
+    </div>
+</main>
 
 <?php
 include("../includes/footer.php");
