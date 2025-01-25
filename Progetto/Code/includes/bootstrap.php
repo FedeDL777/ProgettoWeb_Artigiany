5b221503c8d09b5ce98a25b793fa344f585d3847
Federico De Leonardis <federico.deleonardis@studio.unibo.it>
2025-01-25 14:57:25 +0100

    tolto casino

--- Progetto/Code/includes/bootstrap.php
+++ Progetto/Code/includes/bootstrap.php
@@ -9,4 +9,3 @@ define("UPLOAD_DIR", "./upload/");
 require_once("../database/Database.php");
 require_once("functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "db_artigiany", 3306);
-insertAdmin($dbh);
