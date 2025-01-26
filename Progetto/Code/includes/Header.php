2dd9950a3ac1f115cf92cf9b60b87e6572a93502
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2025-01-26 20:52:09 +0100

    Update author metadata and correct project title in header

--- Progetto/Code/includes/Header.php
+++ Progetto/Code/includes/Header.php
@@ -8,8 +8,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <meta name="description" content="L'artigianalità italiana a casa tua!" />
-    <meta name="author" content="Pietro Sbaraccani, Baloons, Alex Guerrini" />
-    <title>Artigiany</title>
+    <meta name="author" content="Pietro Sbaraccani, Federico De Leonardis, Alex Guerrini" />
+    <title>Artigian-e</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/styles.css">
